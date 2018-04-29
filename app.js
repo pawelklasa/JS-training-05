2385258e486feb4eae38c17cf5196b6c5ec30d46
Pawel <pawel.klasa@gmail.com>
2018-04-29 10:23:33 +0100

    set up

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+// Person constructor
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
 
+// Greeting
+Person.prototype.greeting = function() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+}
+
+const person1 = new Person('John', 'Doe');
+
+console.log(person1);
+
+// Customer sonstructor
+function Customer(firstName, lastName, phone, membership) {
+     
+}
