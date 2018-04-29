90b01b52f25169591f4a61bc564da512818cdcb9
Pawel <pawel.klasa@gmail.com>
2018-04-29 17:34:21 +0100

    finish

---
+++ app.js
@@ -0,0 +1,15 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  }
+}
+
+const mary = new Person('Mary', 'Williams');
+
+console.log(mary.greeting());
+
